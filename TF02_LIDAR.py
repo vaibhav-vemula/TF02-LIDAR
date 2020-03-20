9498b6a7c7c77d8fc9dd3ee440f6d12ae3efe3c4
Vaibhav V <vaibhav25vemula23@gmail.com>
2020-03-20 14:55:53 +0530

    Create TF02_LIDAR.py

---
+++ TF02_LIDAR.py
@@ -0,0 +1,25 @@
+import serial
+
+da=serial.Serial("/dev/ttyTHS1", 115200)
+
+def data():
+    while True:
+        
+        count =da.in_waiting
+        if(count>=8):
+            value=da.read(9)   
+            da.reset_input_buffer() 
+          
+            if(value[0]=='0x59' and value[1]=='0x59'):   
+                distance=value[2]+value[3]*256 
+                print('DISTANCE = ',distance)
+                da.reset_input_buffer()
+                
+if __name__ == '__main__':
+    try:
+        if da.is_open == False:
+            da.open()
+        data()
+    except KeyboardInterrupt:
+        if da!= None:
+            da.close()
