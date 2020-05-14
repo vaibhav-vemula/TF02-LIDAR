0bd088cfabca2dd85a7d8c2cc995b87f80ed5eab
Vaibhav V <vaibhav25vemula23@gmail.com>
2020-05-14 13:00:14 +0530

    Update TF02_LIDAR.py

--- TF02_LIDAR.py
+++ TF02_LIDAR.py
@@ -1,25 +1,25 @@
 import serial
 
-da=serial.Serial("/dev/ttyTHS1", 115200)
+d=serial.Serial("/dev/ttyTHS1", 115200)
 
 def data():
     while True:
         
-        count=da.in_waiting
+        count=d.in_waiting
         if(count>=8):
-            value=da.read(9)   
-            da.reset_input_buffer() 
+            value=d.read(9)   
+            d.reset_input_buffer() 
           
             if(value[0]=='0x59' and value[1]=='0x59'):   
                 distance=value[2]+value[3]*256 
                 print('DISTANCE = ',distance)
-                da.reset_input_buffer()
+                d.reset_input_buffer()
                 
 if __name__ == '__main__':
     try:
-        if da.is_open == False:
-            da.open()
+        if d.is_open == False:
+            d.open()
         data()
     except KeyboardInterrupt:
-        if da!= None:
-            da.close()
+        if d!= None:
+            d.close()
